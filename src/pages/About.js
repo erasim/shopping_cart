0e9bab4cc5242b9a41c34786f39042f1badf9316
erasim.zaidi <erasim.zaidi@gmail.com>
2022-11-21 16:56:46 +0530

    responsive

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,18 +5,10 @@ export default function About() {
   return (
     <div className='Rightbox'>
 
-<div >
-    <h2>Welcome to the About PAge!</h2>
-    <p>You can do this, I believe in you.</p>
-    <p>You can do this, I believe in you.</p>
-    <p>You can do this, I believe in you.</p>
-    <p>You can do this, I believe in you.</p>
-    <p>You can do this, I believe in you.</p>
-    <p>You can do this, I believe in you.</p>
-    <p>You can do this, I believe in you.</p><p>You can do this, I believe in you.</p>
-    
 
-  </div>
+    <h2>Welcome to My Shopping Cart</h2>
+
+  
 
     </div>
   
